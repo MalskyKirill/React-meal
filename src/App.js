5d8876fa52d7c30287599582894b03b5326b43b8
Kirill Malskiy <sabbat@mail.ru>
2023-01-12 15:16:59 +0300

    установил gh-pages для деплоя, и добавил свойство basename в Router

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {Recipe} from './pages/Recipe'
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/React-meal'>
         <Header />
         <main className='container content'>
           <Switch>
